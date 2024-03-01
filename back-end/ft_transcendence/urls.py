2b84486b03cc6781f16d275e05c1c07b32d1266f
zakarm <zakariaemrabet1@gmail.com>
2024-03-01 21:20:09 +0100

    add rest_framwork, auth apps to setting.py and create docker-compose file to link between back-end and database

--- back-end/ft_transcendence/urls.py
+++ back-end/ft_transcendence/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
+    path('', include('authentication.urls')),
 ]
