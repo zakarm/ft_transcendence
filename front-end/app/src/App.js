26533edd6037155b734b10991eef544005d96475
Othmane Nouakchi <onouakch@e3r8p18.1337.ma>
2024-03-05 19:30:31 +0100

    setup routes

--- front-end/app/src/App.js
+++ front-end/app/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Chat from "./pages/Chat";
+import Game from "./pages/Game";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import SideBar from "./Layouts/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <Router>
+          <div class="col-2">
+            <SideBar />
+          </div>
+          <div class="col-8">
+            <Routes>
+              <Route path='/'         element={<Home />}      />
+              <Route path='/chat'     element={<Chat />}      />
+              <Route path='/settings' element={<Settings />}  />
+              <Route path='/game'     element={<Game />}      />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
