47cfe76573ab376b76b7d73eb6ca3855443e1e13
Othmane Nouakchi <onouakch@e3r4p10.1337.ma>
2024-03-06 19:42:56 +0100

    [Fix] Almost finish side bar

--- front-end/app/src/Layouts/Sidebar.js
+++ front-end/app/src/Layouts/Sidebar.js
@@ -1,29 +1,31 @@
 
 import { Link } from 'react-router-dom';
 
-import { RxDashboard } from "react-icons/rx";
-import { LiaGamepadSolid } from "react-icons/lia";
-import { IoChatbubblesOutline , IoSettingsOutline , IoTrophy } from "react-icons/io5";
+import { BiSolidDashboard } from "react-icons/bi";
+import { GiGamepad } from "react-icons/gi";
+import { IoChatbubblesSharp , IoSettings , IoTrophy } from "react-icons/io5";
 import { AiOutlineLogout } from "react-icons/ai";
-import { GiPingPongBat } from "react-icons/gi";
+// import { GiPingPongBat } from "react-icons/gi";
 import './Sidebar.css'
 
 function SideBar() {
   return (
-    <div class="items">
-        <div class="logo">
-          <GiPingPongBat color='#FFFFFF' size='3em'/>
+    <div class="container vh-100">
+      <div class="d-flex flex-column justify-content-around align-items-center vh-100" >
+        <div class="flex-grow-1 d-flex align-items-start pt-3">
+          <img src="yin_yang.jpg" class="img-thumbnail" alt="ying" />
         </div>
-        <div class="nav">
-          <Link class="nav-item" to="/"><RxDashboard color='#FFFFFF' size='3em'/></Link>
-          <Link class="nav-item" to="/game"><LiaGamepadSolid color='#FFFFFF' size='3em'/></Link>
-          <Link class="nav-item" to="/chat"><IoChatbubblesOutline color='#FFFFFF' size='3em'/></Link>
-          <Link class="nav-item" to="/settings"><IoSettingsOutline color='#FFFFFF' size='3em'/></Link>
-          <Link class="nav-item" to="/settings"><IoTrophy color='#FFFFFF' size='3em'/></Link>
+        <div class="flex-grow-1 d-flex flex-column justify-content-around align-items-center">
+          <Link class="" to="/"         ><BiSolidDashboard color='#FFEBEB' size='3em'/></Link>
+          <Link class="" to="/game"     ><GiGamepad color='#FFEBEB' size='3em'/></Link>
+          <Link class="" to="/chat"     ><IoChatbubblesSharp color='#FFEBEB' size='3em'/></Link>
+          <Link class="" to="/settings" ><IoSettings color='#FFEBEB' size='3em'/></Link>
+          <Link class="" to="/settings" ><IoTrophy color='#FFEBEB' size='3em'/></Link>
         </div>
-        <div class="logout">
-          <AiOutlineLogout color='#FFFFFF' size='3em'/>
+        <div class="flex-grow-1 d-flex align-items-end pb-3">
+          <AiOutlineLogout color='#FFEBEB' size='3em'/>
         </div>
+      </div>
     </div>
   );
 }
