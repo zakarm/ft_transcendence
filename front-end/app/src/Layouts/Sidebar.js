39b9e015964adb2bbace199914b714e84dc27053
Othmane Nouakchi <onouakch@e3r8p18.1337.ma>
2024-03-06 10:28:28 +0100

    setup routes

--- front-end/app/src/Layouts/Sidebar.js
+++ front-end/app/src/Layouts/Sidebar.js
@@ -1,15 +1,29 @@
 
 import { Link } from 'react-router-dom';
 
+import { RxDashboard } from "react-icons/rx";
+import { LiaGamepadSolid } from "react-icons/lia";
+import { IoChatbubblesOutline , IoSettingsOutline , IoTrophy } from "react-icons/io5";
+import { AiOutlineLogout } from "react-icons/ai";
+import { GiPingPongBat } from "react-icons/gi";
+import './Sidebar.css'
+
 function SideBar() {
   return (
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/chat">Chat</Link></li>
-        <li><Link to="/settings">Settings</Link></li>
-        <li><Link to="/game">Game</Link></li>
-      </ul>
+    <div class="items">
+        <div class="logo">
+          <GiPingPongBat color='#FFFFFF' size='3em'/>
+        </div>
+        <div class="nav">
+          <Link class="nav-item" to="/"><RxDashboard color='#FFFFFF' size='3em'/></Link>
+          <Link class="nav-item" to="/game"><LiaGamepadSolid color='#FFFFFF' size='3em'/></Link>
+          <Link class="nav-item" to="/chat"><IoChatbubblesOutline color='#FFFFFF' size='3em'/></Link>
+          <Link class="nav-item" to="/settings"><IoSettingsOutline color='#FFFFFF' size='3em'/></Link>
+          <Link class="nav-item" to="/settings"><IoTrophy color='#FFFFFF' size='3em'/></Link>
+        </div>
+        <div class="logout">
+          <AiOutlineLogout color='#FFFFFF' size='3em'/>
+        </div>
     </div>
   );
 }
