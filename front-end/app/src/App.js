c35f76c62a1385bed737b5e67112f4b54f934da1
Othmane Nouakchi <onouakch@e3r4p10.1337.ma>
2024-03-06 19:42:56 +0100

    almost finish side bar

--- front-end/app/src/App.js
+++ front-end/app/src/App.js
@@ -9,19 +9,21 @@ import SideBar from "./Layouts/Sidebar";
 
 function App() {
   return (
-    <div class="container">
-      <div class="row">
+    <div class="container-fluid vh-100" style={{backgroundColor: '#000000'}}>
+      <div class="row vh-100 border">
         <Router>
-          <div class="col-2">
+          <div class="col-1 rounded-5 rounded-start-0" style={{backgroundColor: '#161625'}}>
             <SideBar />
           </div>
-          <div class="col-8">
+          <div class="col-9">
             <Routes>
               <Route path='/'         element={<Home />}      />
               <Route path='/chat'     element={<Chat />}      />
               <Route path='/settings' element={<Settings />}  />
               <Route path='/game'     element={<Game />}      />
             </Routes>
+          </div>
+          <div class="col-2 border border-warning">
           </div>
         </Router>
       </div>
