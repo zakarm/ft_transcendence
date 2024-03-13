b82870cc06867706552f41a04bead30e0db75335
Othmane Nouakchi <onouakch@e3r11p8.1337.ma>
2024-03-13 23:30:10 +0000

    [FEAT] Working on the rightBar

---
+++ front-end/app/src/Layouts/Rightbar.js
@@ -0,0 +1,17 @@
+import { RiNotification4Fill } from "react-icons/ri";
+
+function RightBar() {
+    return (
+        <div className="conatinaer">
+            <div className="row"><RiNotification4Fill /></div>
+            <div className="row"></div>
+            <div className="row"></div>
+            <div className="row"></div>
+            <div className="row"></div>
+            <div className="row"></div>
+            <div className="row"></div>
+        </div>
+    );
+}
+
+export default RightBar;
