a8a5c4fe82dd450205795eadc96b34a67aeabdb3
Othmane Nouakchi <onouakch@e3r2p9.1337.ma>
2024-03-13 19:32:25 +0000

    [FEAT] Create buttons coomponent

--- front-end/app/src/pages/Auth/SignUp.js
+++ front-end/app/src/pages/Auth/SignUp.js
@@ -1,7 +1,6 @@
 
 import Image from 'react-bootstrap/Image';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import { FaGithub , FaGoogle } from "react-icons/fa";
 import { Si42 } from "react-icons/si";
 import './SignUp.css'
@@ -10,34 +9,34 @@ function SignUp()
 {
     return (
         <div className="container" style={{backgroundColor: '#111111'}}>
-            <div className="conatiner" style={{backgroundColor: '#161625'}}>
+            <div className="conatiner p-2" style={{backgroundColor: '#161625'}}>
                 <div className="row">
-                    <div className="col border">
+                    <div className="col ">
                         <Image className='' src="assets/char2.png" fluid />    
                     </div>
-                    <div className="col border  align-self-center text-center">
-                        <div className='row border'><h1 className='valo-font'>VAL-PONG</h1></div>
+                    <div className="col   align-self-center text-center">
+                        <div className='row '><h1 className='valo-font'>VAL-PONG</h1></div>
                         <div>
-                            <div className="row border mb-3">
+                            <div className="row  mb-3">
                                 <Form.Control className="col p-3 m-2 mb-0" placeholder='first name' data-bs-theme="dark" style={{fontFamily: "itim", backgroundColor: '#2C3143'}}/>
                                 <Form.Control className="col p-3 m-2 mb-0" placeholder='last name' data-bs-theme="dark" style={{fontFamily: "itim", backgroundColor: '#2C3143'}}/>
                             </div>
-                            <div className="row border p-2 pt-0">
+                            <div className="row  p-2 pt-0">
                                 <input className="form-control p-3 mb-3" type='text' placeholder='nickname' data-bs-theme="dark" style={{fontFamily: "itim", backgroundColor: '#2C3143'}}/>
                                 <input className="form-control p-3 mb-3" type='text' placeholder='email' data-bs-theme="dark" style={{fontFamily: "itim", backgroundColor: '#2C3143'}}/>
                                 <input className="form-control p-3 mb-3" type='text' placeholder='password' data-bs-theme="dark" style={{fontFamily: "itim", backgroundColor: '#2C3143'}}/>
                             </div>
                         </div>
-                        <div className='row border p-2 d-flex justify-content-center'>
-                            <Button className='sign-btn col-md-3 btn-lg m-2 p-3' type='button' >SIGN UP</Button>
+                        <div className='row  p-2 d-flex justify-content-center'>
+                            <button className='sign-btn col-md-3 btn-lg m-2 p-3' type='button' >SIGN UP</button>
                             <Form.Label className='' style={{fontFamily: "itim", color: '#565A69'}}>Already have an account? <a href="/home" style={{color: '#FF4755', fontFamily: 'itim'}}>sign in</a></Form.Label>
                         </div>
-                        <div className='row text-start border'>
+                        <div className='row text-start '>
                             <Form.Label style={{fontFamily: "itim"}}>or sign up with :</Form.Label>
-                            <div className=''>
-                                <Button className='' variant="flat" size="xxl" style={{backgroundColor: '#161625', border: "1px solid #2C3143" }}><FaGithub color='#FFEBEB'/></Button>
-                                <Button className='' variant="flat" size="xxl" style={{backgroundColor: '#161625', border: "1px solid #2C3143" }}><FaGoogle color='#FFEBEB'/></Button>
-                                <Button className='' variant="flat" size="xxl" style={{backgroundColor: '#161625', border: "1px solid #2C3143" }}><Si42 color='#FFEBEB'/></Button>
+                            <div className='d-flex justify-content-around align-items-center'>
+                                <button className='auth-btn ' type='button' ><FaGithub color='#FFEBEB'/></button>
+                                <button className='auth-btn ' type='button' ><FaGoogle color='#FFEBEB'/></button>
+                                <button className='auth-btn ' type='button' ><Si42 color='#FFEBEB'/></button>
                             </div>
                         </div>
                     </div>
