d0af4ed6c307e67d4813c450c1e2d43746890af0
Nouakchi <Othman.nouakchi007@gmail.com>
2024-03-14 20:36:14 +0000

    [Feat] addin notification dropdown

--- front-end/app/src/Layouts/Rightbar.js
+++ front-end/app/src/Layouts/Rightbar.js
@@ -1,9 +1,18 @@
 import { RiNotification4Fill } from "react-icons/ri";
-
+import './Rightbar.css'
 function RightBar() {
     return (
         <div className="conatinaer">
-            <div className="row"><RiNotification4Fill /></div>
+            <div className="row">
+                <div className="col">
+                {/* <a href="/" class="notification border">    */}
+                <div className="notification">
+                    <RiNotification4Fill color="#FF4755" size='1.5em'/>
+                    <span class="badge">3</span>
+                </div>
+                {/* </a> */}
+                </div>
+            </div>
             <div className="row"></div>
             <div className="row"></div>
             <div className="row"></div>
