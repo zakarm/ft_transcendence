5fc605f6750d0cbe487c500adf5dbe01d3b60353
Nouakchi <Othman.nouakchi007@gmail.com>
2024-03-26 04:13:20 +0000

    [Feat] Responsive right bar is finished

--- front-end/app/src/Layouts/SrightBar.js
+++ front-end/app/src/Layouts/SrightBar.js
@@ -38,51 +38,43 @@ function SrightBar(props) {
         <Splayer key={index} nickname={user.nickname} id={user.id} image={user.image_url} isConnected={user.connected}/>
     );
     return (
-        <div className='row-fluid d-flex flex-row align-items-center p-0 vh-100'>
-            <div className="container vh-100 p-0">
-                <div className="d-flex flex-column vh-100">
-                    <div className="flex-grow-2 pb-1">
-                        <div className="row-inline align-items-center text-center">
-                            <div className="row m-0">
-                                <div className='d-flex flex-column align-items-center p-2'>
-                                    <div className='holder p-2'>
-                                        <div className="col notification1">
-                                           <Dropdown>
-                                               <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-                                                    <img className="img-class1" src="assets/char3.png" alt='Profile'/>
-                                                    <span className="badge1">3</span>
-                                               </Dropdown.Toggle>
-                                               <Dropdown.Menu className="drop-class">
-                                                 <Dropdown.Item eventKey="1"><Notification /></Dropdown.Item>
-                                                 <Dropdown.Item eventKey="2"><Notification /></Dropdown.Item>
-                                                 <Dropdown.Item eventKey="3"><Notification /></Dropdown.Item>
-                                               </Dropdown.Menu>
-                                           </Dropdown>
-                                        </div>
-                                        <div className="col pt-1">
-                                             <h5 className="valo-font">Me</h5>
-                                        </div>
+            <div className="d-flex flex-column vh-100 py-2">
+                <div className="pb-1" style={{width: '91%'}}>
+                            <div className=' d-flex flex-column align-items-center justify-content-center p-0'>
+                                <div className='holder text-center p-2'>
+                                    <div className="col-inline notification1">
+                                       <Dropdown>
+                                           <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                                                <img className="img-class1" src="assets/char3.png" alt='Profile'/>
+                                                <span className="badge1">3</span>
+                                           </Dropdown.Toggle>
+                                           <Dropdown.Menu className="drop-class">
+                                             <Dropdown.Item eventKey="1"><Notification /></Dropdown.Item>
+                                             <Dropdown.Item eventKey="2"><Notification /></Dropdown.Item>
+                                             <Dropdown.Item eventKey="3"><Notification /></Dropdown.Item>
+                                           </Dropdown.Menu>
+                                       </Dropdown>
+                                    </div>
+                                    <div className="usr col pt-1">
+                                         <h5 className="valo-font">Me</h5>
                                     </div>
                                 </div>
-                             </div>
-                             <div className='row d-felx justify-content-center m-0'>
-                                <div className="col-6 search-inpt1 p-2 m-2" onClick={props.toggleShow}>
-                                     <IoIosSearch color="#FFEBEB" size='2em'/>
-                                </div>
-                             </div>
-                        </div>
-                    </div>
-                    <div className=" mb-2 flex-grow-1 text-center" style={{ overflowY: 'auto' }}>
-                        {friendsData}
-                    </div>
-                    <div className="flex-grow-3 row-inline">
-                        <div className="col-10 search-inpt p-2 mb-4 mx-2 text-center" style={{cursor: "pointer"}}>
-                                <ImUserPlus color="#FFEBEB" size='2em'/>
-                        </div>
+                            </div>
+                         <div className='row d-flex text-center justify-content-center m-0'>
+                            <div className="col-6 search-inpt1 p-2 m-2" onClick={props.toggleShow}>
+                                 <IoIosSearch color="#FFEBEB"/>
+                            </div>
+                         </div>
+                </div>
+                <div className=" mb-2 flex-grow-1 text-center" style={{ overflowY: 'auto', width: '91%' }}>
+                    {friendsData}
+                </div>
+                <div className="flex-grow-3 row-inline d-flex justify-content-center text-center" style={{width: '91%'}}>
+                    <div className="col-6 search-inpt my-1 p-2  mx-2 text-center" style={{cursor: "pointer"}}>
+                            <ImUserPlus color="#FFEBEB"/>
                     </div>
                 </div>
             </div>
-        </div>
     );
 }
 
