3d93ce47b98f31bc5f995f8a5760e23b40739dfe
zakarm <zakariaemrabet1@gmail.com>
2024-04-24 20:01:05 +0100

    [Feat] Test pylint

--- app/back-end/authentication/admin.py
+++ app/back-end/authentication/admin.py
@@ -11,7 +11,6 @@ class UserAdmin(admin.ModelAdmin):
                      'date_joined', 'is_superuser', 'last_login',
                      'image_url', 'cover_url', 'location')
     readonly_fields = ('id', 'date_joined', 'last_login')
-
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
