29e1dd8269e96c22957dbbe897762a870ebae5b1
Marouane <procompte97@gmail.com>
2024-05-01 17:39:05 +0100

    [FIX] adds upload file type

--- app/front-end/src/app/game/(tournament-form)/form.tsx
+++ app/front-end/src/app/game/(tournament-form)/form.tsx
@@ -9,7 +9,8 @@ interface Props{
     className ?: string;
     inputClassName ?: string;
     inputId ?:string;
-    labelText ?:string
+    labelText ?:string,
+    labelClass ?: string
 }
 
 function    GetInput(
@@ -19,13 +20,18 @@ function    GetInput(
         inputType="text",
         placeholder="",
         inputId="",
-        labelText=""
+        labelText="",
+        labelClass=""
     }: Props) {
     return (
         <>
         <div className={`${className}`}>
-            <label className="col-9 itim-font text-left p-0 m-0" for={inputId}>{labelText}</label>
-            <div className={`${styles.inputHolder} row justify-content-center p-0 m-2`}>
+            <label
+                className={`col-9 itim-font text-left p-0 m-0 ${styles.inputTitle} ${styles.labelClass}`} 
+                htmlFor={inputId}>
+                    {labelText}
+            </label>
+            <div className={`col-9 ${styles.inputHolder} row justify-content-center p-0 m-2`}>
                 <input
                     type={inputType}
                     placeholder={placeholder}
@@ -37,6 +43,42 @@ function    GetInput(
         </>
     )
 }
+
+function    GetImageInput({}: Props) {
+    return (
+        <>
+        <div className={`p-2 mt-2 row justify-content-center`}>
+            <label className={`col-9 ${styles.inputTitle} itim-font mb-2 p-0`} htmlFor="imageInput">Tournament Image</label>
+            <label
+                className={`col-9 itim-font text-end ${styles.input} ${styles.imageFile}`}
+                htmlFor="imageInput">.jpg</label>
+
+            <div className={`${styles.inputHolder} row justify-content-center p-0 m-2`}>
+                <input
+                    type="file"
+                    placeholder="upload an image"
+                    className={`imageInput`}
+                    id="imageInput"
+                />
+            </div>
+        </div>
+        </>
+    )
+}
+
+function    InputRange() {
+    return (
+        <>
+        <div class={`row justify-content-center ${styles.slidecontainer} p-0 mt-2`}>
+            <label className={`col-9 itim-font text-left p-1 ms-4 ${styles.inputTitle}`} for="myRange">Game Difficulty</label>=
+            <div className="col-9 d-flex justify-content-center p-0 my-3">
+                <input type="range" min="0" max="12" step="6" class={`${styles.slider}`} id="myRange"/>
+            </div>
+        </div>
+        </>
+    )
+}
+
 
 function CreateTournament() {
     return (
@@ -53,39 +95,28 @@ function CreateTournament() {
 
                     <GetInput
                         className="p-2 mt-2 row justify-content-center"
-                        inputClassName="inputForm"
+                        inputClassName=""
                         inputType="text"
                         inputId="Username"
                         labelText="Username">
                     </GetInput>
                     <GetInput
                         className="p-2 mt-2 row justify-content-center"
-                        inputClassName="inputForm"
+                        inputClassName=""
                         inputType="text"
                         inputId="Tournament_name"
                         labelText="Tournament name">
                     </GetInput>
                     <GetInput
                         className="p-2 mt-2 row justify-content-center"
-                        inputClassName="inputForm"
+                        inputClassName=""
                         inputType="text"
                         inputId="start_date"
                         labelText="Start date">
                     </GetInput>
-                    <GetInput
-                        className="p-2 mt-2 row justify-content-center"
-                        inputClassName="inputForm"
-                        inputType="text"
-                        inputId="end_date"
-                        labelText="End date">
-                    </GetInput>
+                    <GetImageInput></GetImageInput>
                 </div>
-                <div class={`row justify-content-center ${styles.slidecontainer} p-0 mt-2`}>
-                    <label className="col-9 itim-font text-left p-0 m-0" for="myRange">Game Difficulty</label>=
-                    <div className="col-9 d-flex justify-content-center p-0 my-3">
-                        <input type="range" min="0" max="12" step="6" class={`${styles.slider}`} id="myRange"/>
-                    </div>
-                </div>
+                <InputRange></InputRange>
             </filedset>
 
             <div className="row p-0 my-4">
