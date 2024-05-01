053bf084c0ae9f0f91cf9e3f9915339a1cf65ffb
zakarm <zakariaemrabet1@gmail.com>
2024-05-01 16:01:05 +0100

    [Feat] Add remove friend end-point

--- app/back-end/dashboards/urls.py
+++ app/back-end/dashboards/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (MainDashboardView,
                     ProfileView,
                     ProfileIdView,
-                    FriendsView)
+                    FriendsView,
+                    RemoveFriendshipView)
 
 urlpatterns = [
    path('dashboard', MainDashboardView.as_view(), name='dashboard'),
    path('profile', ProfileView.as_view(), name='profile'),
-   path('profile/<int:id>', ProfileIdView.as_view(), name='profile_with_id'),
-   path('friends', FriendsView.as_view(), name='friends')
+   path('profile/<str:username>', ProfileIdView.as_view(), name='profile_with_id'),
+   path('friends', FriendsView.as_view(), name='friends'),
+   path('friends-remove', RemoveFriendshipView.as_view(), name="friends-remove")
 ]
--- app/back-end/dashboards/views.py
+++ app/back-end/dashboards/views.py
@@ -5,9 +5,11 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializer import (MainDashboardSerializer,
                          ProfileSerializer,
                          FriendsSerializer)
+from .models import Friendship
 from authentication.models import User
 from rest_framework import status
 import sys
+from django.db.models import F, Q
  
 class MainDashboardView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -49,3 +51,24 @@ class FriendsView(APIView):
         user = request.user
         serializer = FriendsSerializer(instance=user)
         return Response(serializer.data)
+
+class RemoveFriendshipView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user_from = request.user
+        username = request.data.get('username')
+
+        try:
+            user_remove = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            friendship = Friendship.objects.get(Q(user_from=user_from, user_to=user_remove)|
+                                                Q(user_from=user_remove, user_to=user_from))
+            friendship.delete()
+            return Response({'success': 'Friendship removed'}, status=status.HTTP_200_OK)
+        except Friendship.DoesNotExist:
+            return Response({'error': 'Friendship does not exist'}, status=status.HTTP_400_BAD_REQUEST)
