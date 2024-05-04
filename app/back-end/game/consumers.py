bb93cd2793219ba1764be8104d5756366fe6cde9
ael-mouz <ael-mouz@student.1337.ma>
2024-05-04 23:10:39 +0100

    [feat] complete remote pong game logic and reorganize code

--- app/back-end/game/consumers.py
+++ app/back-end/game/consumers.py
@@ -1,13 +1,18 @@
-from django.contrib.auth.models import AnonymousUser
+# Standard library imports
+import asyncio
+import json
+import random
+import sys
+
+# Third-party imports
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-import json
-import sys
-from .room import RoomObject
-from django.utils import timezone
 from django.contrib.auth import get_user_model
-import asyncio
-import random
+from django.contrib.auth.models import AnonymousUser
+from django.utils import timezone
+
+# Local application/library specific imports
+from .room import RoomObject
 
 User = get_user_model()
 
@@ -31,22 +36,22 @@ class GameConsumer(AsyncWebsocketConsumer):
     Rooms_index = 0
     rooms = {}
 
-    async def get_room(self):
+    # -----------------------> 0. get_room <-----------------------
+    def get_room(self):
         return self.rooms[self.room_name]
 
     # -----------------------> 1. broadcast_message <-----------------------
     async def broadcast_message(self, message):
         try:
-            print(f"Broadcasting message: {message}", file=sys.stderr)
             await self.channel_layer.group_send(
                 self.room_name, {"type": "message", "message": message}
             )
         except Exception as e:
             print(f"An error occurred in broadcast_message: {e}")
 
+    # -----------------------> 2. message <-----------------------
     async def message(self, event):
         try:
-            print(f"Received message: {event['message']}", file=sys.stderr)
             message = event["message"]
             await self.send(
                 text_data=json.dumps({"message": message, "time": current_time()})
@@ -54,15 +59,16 @@ class GameConsumer(AsyncWebsocketConsumer):
         except Exception as e:
             print(f"An error occurred in message: {e}")
 
-    # -----------------------> 2. connect <-----------------------
-
+    # -----------------------> 3. connect <-----------------------
     async def connect(self):
         try:
-            print("-" * 50, file=sys.stderr)
             if self.scope["user"].is_authenticated:
                 await self.accept()
+                print(f"Connected to {self.channel_name}", file=sys.stderr)
                 self.user = await get_user(user_id=self.scope["user"].id)
+                print(f"User: {self.user}", file=sys.stderr)
                 self.room_name, self.room = await self.find_or_create_room(self.user)
+                print(f"Room_name: {self.room_name}", file=sys.stderr)
                 await self.channel_layer.group_add(self.room_name, self.channel_name)
                 index = self.room.get_user_index(self.user)
                 message = f"action: connection_ack, index: {index}, User: {self.user}, Room_name: {self.room_name}"
@@ -78,64 +84,109 @@ class GameConsumer(AsyncWebsocketConsumer):
         except Exception as e:
             print(f"An error occurred in connect: {e}")
 
+    # -----------------------> 4. disconnect <-----------------------
+    async def disconnect(self, close_code):
+        # await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        pass
+
+    # -----------------------> 5. receive <-----------------------
+    async def receive(self, text_data):
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json["message"]
+            print(f"Received message: {message}")
+        except Exception as e:
+            print(f"An error occurred in receive: {e}")
+
+    # -----------------------> 6. start_game <-----------------------
     async def start_game(self):
         try:
-            i = 0
             await asyncio.sleep(5)
             await self.broadcast_message(f"action: load_game")
             await asyncio.sleep(5)
             await self.broadcast_message(f"action: start_game")
             await self.init_pos()
+            room = self.get_room()
             while True:
-                i += 1
-                message = f"i = {i}"
-                await self.broadcast_message(message)
-                await asyncio.sleep(1)
-
+                room.ball_update()
+                room.ball_intersect()
+                room.paddle_update()
+                if room.is_paddle_move(1):
+                    message = f"action: paddle_update, paddle: 1, paddle_position_z : {room.paddle1_position['z']}"
+                    await self.broadcast_message(message)
+                if room.is_paddle_move(2):
+                    message = f"action: paddle_update, paddle: 2, paddle_position_z : {room.paddle2_position['z']}"
+                    await self.broadcast_message(message)
+                if room.is_out_of_bounds():
+                    room.update_score()
+                    message = f"action: score, user1score: {room.getScores()['user1']}, user2score: {room.getScores()['user2']}"
+                    await self.broadcast_message(message)
+                    if room.is_winner():
+                        room.end_game()
+                        winner, loser = room.get_winner()
+                        message = f"action: end_game, winner: {winner}, loser: {loser}"
+                        await self.broadcast_message(message)
+                        break
+                    room.paddle_reset()
+                    ball_pos_z, ball_velo_x, ball_velo_z = await self.reset()
+                    message = f"action: reset, ball_position_x: {0}, ball_position_z: {ball_pos_z}, ball_velocity_x: {ball_velo_x}, ball_velocity_z: {ball_velo_z}"
+                    await self.broadcast_message(message)
+                else:
+                    ball_position, ball_velocity = room.get_updated_ball()
+                    message = f"action: update,  ball_position_x: {ball_position['x']}, ball_position_z: {ball_position['z']}, ball_velocity_x: {ball_velocity['velocity_x']}, ball_velocity_z: {ball_velocity['velocity_x']}"
+                    await self.broadcast_message(message)
+                await asyncio.sleep(1 / 60)
         except Exception as e:
             print(f"An error occurred in connect: {e}")
 
+    # -----------------------> 7. init_pos <-----------------------
     async def init_pos(self):
-        room = await self.get_room()
-        ball_position_z = random.uniform(-2.2, 2.2)
-        ball_velocity_x = 0.05 * random.choice([-1, 1])
-        ball_velocity_z = 0.05 * random.choice([-1, 1])
-        room.set_ball_position(0, ball_position_z)
-        room.set_ball_velocity(ball_velocity_x, ball_velocity_z)
-        room.start_game()
-        room.set_game_state("playing")
-        return ball_position_z, ball_velocity_x, ball_velocity_z
-
-    # -----------------------> 3. disconnect <-----------------------
-    async def disconnect(self, close_code):
-        # await self.channel_layer.group_discard(self.room_name, self.channel_name)
-        pass
-
-    # -----------------------> 4. receive <-----------------------
-    async def receive(self, text_data):
-        print(f"Received message: {text_data}", file=sys.stderr)
         try:
-            text_data_json = json.loads(text_data)
-            message = text_data_json["message"]
-        except json.JSONDecodeError:
-            message = text_data
-        await self.message({"message": message})
+            room = self.get_room()
+            ball_position_z = random.uniform(-2.2, 2.2)
+            ball_velocity_x = 0.05 * random.choice([-1, 1])
+            ball_velocity_z = 0.05 * random.choice([-1, 1])
+            room.set_ball_position(0, ball_position_z)
+            room.set_ball_velocity(ball_velocity_x, ball_velocity_z)
+            room.start_game()
+            return ball_position_z, ball_velocity_x, ball_velocity_z
+        except Exception as e:
+            print(f"An error occurred in init_pos: {e}")
+
+    # -----------------------> 8. reset <-----------------------
+    async def reset(self):
+        try:
+            room = self.get_room()
+            ball_position_z = random.uniform(-2.2, 2.2)
+            if room.ball_position["x"] < 0:
+                ball_velocity_x = 0.05
+            else:
+                ball_velocity_x = -0.05
+            ball_velocity_z = 0.05 * random.choice([-1, 1])
+            room.set_ball_position(0, ball_position_z)
+            room.set_ball_velocity(ball_velocity_x, ball_velocity_z)
+            return ball_position_z, ball_velocity_x, ball_velocity_z
+        except Exception as e:
+            print(f"An error occurred in reset: {e}")
 
     # -----------------------> 5. find_or_create_room <-----------------------
     async def find_or_create_room(self, user_id):
-        for room_name, room in self.rooms.items():
-            if room.get_game_state() == True and room.is_user_joined(user_id):
-                room.reconecting_user(self.channel_name, user_id)
-                await self.message({"message": "action: reconected"})
-                return room_name, room
-            elif room.get_game_state() == False and not room.is_user_joined(user_id):
-                room.add_user(self.channel_name, user_id)
-                await self.message({"message": "action : joined"})
-                return room_name, room
-        self.Rooms_index += 1
-        new_room_name = f"room_{self.Rooms_index}"
-        new_room = RoomObject()
-        new_room.add_user(self.channel_name, user_id)
-        await self.message({"message": "action: created + joined"})
-        self.rooms[new_room_name] = new_room
-        return new_room_name, new_room
+        try:
+            for room_name, room in self.rooms.items():
+                if room.is_started() == True and room.is_user_joined(user_id):
+                    room.reconecting_user(self.channel_name, user_id)
+                    await self.message({"message": "action: reconected"})
+                    return room_name, room
+                elif room.is_started() == False and not room.is_user_joined(user_id):
+                    room.add_user(self.channel_name, user_id)
+                    await self.message({"message": "action : joined"})
+                    return room_name, room
+            self.Rooms_index += 1
+            new_room_name = f"room_{self.Rooms_index}"
+            new_room = RoomObject()
+            new_room.add_user(self.channel_name, user_id)
+            await self.message({"message": "action: created + joined"})
+            self.rooms[new_room_name] = new_room
+            return new_room_name, new_room
+        except Exception as e:
+            print(f"An error occurred in find_or_create_room: {e}")
--- app/back-end/game/room.py
+++ app/back-end/game/room.py
@@ -1,11 +1,16 @@
-import sys
-
 
 class RoomObject:
     def __init__(self):
+        # game state
         self.game_started = False
-        self.game_state = "waiting"
+        self.index = 1
+
+        # ball
         self.ball_radius = 0.1
+        self.ball_position = {"x": 0, "z": 0}
+        self.ball_velocity = {"velocity_x": 0, "velocity_z": 0}
+
+        # paddle
         self.paddle_width = 0.2
         self.paddle_height = 0.2
         self.paddle_depth = 1
@@ -13,21 +18,50 @@ class RoomObject:
         self.paddle2_speed = 0
         self.paddle1_position = {"x": -4.8, "z": 0}
         self.paddle2_position = {"x": 4.8, "z": 0}
-        self.ball_position = {"x": 0, "z": 0}
-        self.ball_velocity = {"velocity_x": 0, "velocity_z": 0}
+
+        # users
         self.Original_users = {
             "user1": {"joined": 0, "user_id": "", "index": 1, "channel_name": ""},
             "user2": {"joined": 0, "user_id": "", "index": 2, "channel_name": ""},
         }
-        # self.users = []
-        self.index = 1
         self.score = {"user1": 0, "user2": 0}
 
-    def set_game_state(self, state):
-        self.game_state = state
-
+    # ------------------------> game <------------------------
     def start_game(self):
         self.game_started = True
+
+    def end_game(self):
+        self.game_started = False
+
+    def is_started(self):
+        return self.game_started
+    # ------------------------> user <------------------------
+    def get_winner(self):
+        if self.score["user1"] == 7:
+            return self.Original_users["user1"]["user_id"] , self.Original_users["user2"]["user_id"]
+        elif self.score["user2"] == 7:
+            return self.Original_users["user2"]["user_id"] , self.Original_users["user1"]["user_id"]
+        return None
+
+    def is_winner(self):
+        if(self.score["user1"] == 7 or self.score["user2"] == 7):
+            return True
+        return False
+
+    def update_score(self):
+        if self.ball_position["x"] < -5:
+            self.score["user1"] += 1
+        elif self.ball_position["x"] > 5:
+            self.score["user2"] += 1
+
+    def getScores(self):
+        return self.score
+
+    def is_ready(self):
+        return (
+            self.Original_users["user1"]["joined"] == 1
+            and self.Original_users["user2"]["joined"] == 1
+        )
 
     def get_user_index(self, user_id):
         if self.Original_users["user1"]["user_id"] == user_id:
@@ -41,9 +75,6 @@ class RoomObject:
             self.Original_users["user2"]["user_id"],
         )
 
-    def get_game_state(self):
-        return self.game_started
-
     def is_user_joined(self, user_id):
         if (
             self.Original_users["user1"]["user_id"] == user_id
@@ -51,12 +82,6 @@ class RoomObject:
         ):
             return True
         return False
-
-    def reconecting_user(self, channel_name, user_id):
-        if self.Original_users["user1"]["user_id"] == user_id:
-            self.Original_users["user1"]["channel_name"] = channel_name
-        elif self.Original_users["user2"]["user_id"] == user_id:
-            self.Original_users["user2"]["channel_name"] = channel_name
 
     def add_user(self, channel_name, user_id):
         if self.Original_users["user1"]["joined"] == 0:
@@ -68,6 +93,23 @@ class RoomObject:
             self.Original_users["user2"]["user_id"] = user_id
             self.Original_users["user2"]["joined"] = 1
 
+    def reconecting_user(self, channel_name, user_id):
+        if self.Original_users["user1"]["user_id"] == user_id:
+            self.Original_users["user1"]["channel_name"] = channel_name
+        elif self.Original_users["user2"]["user_id"] == user_id:
+            self.Original_users["user2"]["channel_name"] = channel_name
+
+    # def remove_user(self, user_id):
+    #     if self.Original_users["user1"]["user_id"] == user_id:
+    #         self.Original_users["user1"]["joined"] = 0
+    #         self.Original_users["user1"]["user_id"] = ""
+    #         self.Original_users["user1"]["channel_name"] = ""
+    #     elif self.Original_users["user2"]["user_id"] == user_id:
+    #         self.Original_users["user2"]["joined"] = 0
+    #         self.Original_users["user2"]["user_id"] = ""
+    #         self.Original_users["user2"]["channel_name"] = ""
+
+    # ------------------------> ball <------------------------
     def set_ball_position(self, x, z):
         self.ball_position["x"] = x
         self.ball_position["z"] = z
@@ -81,8 +123,6 @@ class RoomObject:
         self.ball_position["z"] += self.ball_velocity["velocity_z"]
         self.ball_velocity["velocity_x"] *= 1.0005
         self.ball_velocity["velocity_z"] *= 1.0005
-        # self.ball_velocity["velocity_x"] *= 1.00005
-        # self.ball_velocity["velocity_z"] *= 1.00005
 
     def ball_reset(self):
         self.ball_position["x"] = 0
@@ -93,48 +133,8 @@ class RoomObject:
     def get_updated_ball(self):
         return self.ball_position, self.ball_velocity
 
-    def remove_user(self, channel_name):
-        self.users = [
-            user for user in self.users if user["channel_name"] != channel_name
-        ]
-
-    def paddle_update(self):
-        newPosition1 = self.paddle1_position["z"] + self.paddle1_speed
-        if newPosition1 < -2.5 + self.paddle_depth / 2:
-            self.paddle1_position["z"] = -2.5 + self.paddle_depth / 2
-            self.paddle1_speed = 0
-        elif newPosition1 > 2.5 - self.paddle_depth / 2:
-            self.paddle1_position["z"] = 2.5 - self.paddle_depth / 2
-            self.paddle1_speed = 0
-        else:
-            self.paddle1_position["z"] = newPosition1
-        newPosition2 = self.paddle2_position["z"] + self.paddle2_speed
-        if newPosition2 < -2.5 + self.paddle_depth / 2:
-            self.paddle2_position["z"] = -2.5 + self.paddle_depth / 2
-            self.paddle1_speed = 0
-        elif newPosition2 > 2.5 - self.paddle_depth / 2:
-            self.paddle2_position["z"] = 2.5 - self.paddle_depth / 2
-            self.paddle1_speed = 0
-        else:
-            self.paddle2_position["z"] = newPosition2
-
-    def paddle_reset(self):
-        self.paddle1_position["z"] = 0
-        self.paddle2_position["z"] = 0
-        self.paddle1_speed = 0
-        self.paddle2_speed = 0
-
-    def is_paddle_move(self, paddle):
-        if paddle == 1:
-            return self.paddle1_speed != 0
-        elif paddle == 2:
-            return self.paddle2_speed != 0
-
-    def set_paddle_speed(self, paddle, speed):
-        if paddle == 1:
-            self.paddle1_speed = speed
-        elif paddle == 2:
-            self.paddle2_speed = speed
+    def is_out_of_bounds(self):
+        return self.ball_position["x"] < -5 or self.ball_position["x"] > 5
 
     def ball_intersect(self):
         if (
@@ -228,26 +228,41 @@ class RoomObject:
         ):
             self.ball_velocity["velocity_z"] *= -1
 
-    def is_out_of_bounds(self):
-        return self.ball_position["x"] < -5 or self.ball_position["x"] > 5
+    # ------------------------> paddle <------------------------
+    def paddle_update(self):
+        newPosition1 = self.paddle1_position["z"] + self.paddle1_speed
+        if newPosition1 < -2.5 + self.paddle_depth / 2:
+            self.paddle1_position["z"] = -2.5 + self.paddle_depth / 2
+            self.paddle1_speed = 0
+        elif newPosition1 > 2.5 - self.paddle_depth / 2:
+            self.paddle1_position["z"] = 2.5 - self.paddle_depth / 2
+            self.paddle1_speed = 0
+        else:
+            self.paddle1_position["z"] = newPosition1
+        newPosition2 = self.paddle2_position["z"] + self.paddle2_speed
+        if newPosition2 < -2.5 + self.paddle_depth / 2:
+            self.paddle2_position["z"] = -2.5 + self.paddle_depth / 2
+            self.paddle1_speed = 0
+        elif newPosition2 > 2.5 - self.paddle_depth / 2:
+            self.paddle2_position["z"] = 2.5 - self.paddle_depth / 2
+            self.paddle1_speed = 0
+        else:
+            self.paddle2_position["z"] = newPosition2
 
-    def update_score(self):
-        if self.ball_position["x"] < -5:
-            self.score["user1"] += 1
-        elif self.ball_position["x"] > 5:
-            self.score["user2"] += 1
+    def paddle_reset(self):
+        self.paddle1_position["z"] = 0
+        self.paddle2_position["z"] = 0
+        self.paddle1_speed = 0
+        self.paddle2_speed = 0
 
-    def getScores(self):
-        return self.score
+    def is_paddle_move(self, paddle):
+        if paddle == 1:
+            return self.paddle1_speed != 0
+        elif paddle == 2:
+            return self.paddle2_speed != 0
 
-    def is_ready(self):
-        return (
-            self.Original_users["user1"]["joined"] == 1
-            and self.Original_users["user2"]["joined"] == 1
-        )
-
-    def is_empty(self):
-        return not self.users
-
-    def is_started(self):
-        return self.game_started
+    def set_paddle_speed(self, paddle, speed):
+        if paddle == 1:
+            self.paddle1_speed = speed
+        elif paddle == 2:
+            self.paddle2_speed = speed
