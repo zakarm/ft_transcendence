974c9d3b8c90000f34e4ab40272191225b97e439
zakarm <mrabet.zakariae21@ump.ac.ma>
2024-05-07 09:26:29 +0100

    [Fix] Update the style of sign in and sign up page

--- app/front-end/src/app/(authentication)/sign-in/page.tsx
+++ app/front-end/src/app/(authentication)/sign-in/page.tsx
@@ -74,7 +74,7 @@ export default function SignInPage() {
                                     VAL-PONG
                                 </h1>
                             </div>
-                            <form className={`form-group mt-3 mb-3 w-75`} onSubmit={signInPost}>
+                            <form className={`form-group mt-3 mb-3 w-75 ${styles.forml}`} onSubmit={signInPost}>
                                 <div className='mb-3'>
                                     <input className={`form-control ${styles.input_class} p-3 mb-3 border-0`} type='email' autoComplete="off" placeholder='Email' name="email" required/>
                                 </div>
