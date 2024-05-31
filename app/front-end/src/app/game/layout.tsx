d86aad0cde65fca66cdf26c0589b6101bf5e8270
ael-mouz <ael-mouz@student.1337.ma>
2024-05-31 12:40:27 +0100

    [Revert] remove unsuccessful attempt to set WebSocketProvider in game pages

--- app/front-end/src/app/game/layout.tsx
+++ app/front-end/src/app/game/layout.tsx
@@ -2,14 +2,14 @@
 import MainContainer from '../../components/mainContainer';
 import AuthChecker from '../../components/authChecker';
 import React from 'react';
-import { WebSocketProvider } from '@/components/webSocket';
+// import { WebSocketProvider } from '@/components/webSocket';
 
 export default function GameLayout({ children }: { children: React.ReactNode }) {
     return (
         <AuthChecker>
-            <WebSocketProvider>
-                <MainContainer>{children}</MainContainer>
-            </WebSocketProvider>
+            {/* <WebSocketProvider> */}
+            <MainContainer>{children}</MainContainer>
+            {/* </WebSocketProvider> */}
         </AuthChecker>
     );
 }
