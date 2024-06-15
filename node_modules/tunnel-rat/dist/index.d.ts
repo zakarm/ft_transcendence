f0503158ced1d5beff941c44da7aba3b2fa8e27e
zakarm <zakariaemrabet1@gmail.com>
2024-06-15 17:55:52 +0100

    [Fix] Delete node modules folder pushed by mistake

--- node_modules/tunnel-rat/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-declare type Props = {
-    children: React.ReactNode;
-};
-export default function tunnel(): {
-    In: ({ children }: Props) => null;
-    Out: () => JSX.Element;
-};
-export {};
