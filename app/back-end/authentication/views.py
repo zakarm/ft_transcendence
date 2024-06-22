d8f3bdf73ee007307f0fd348c2dfee6e5045a152
zakarm <zakariaemrabet1@gmail.com>
2024-06-22 19:45:38 +0100

    [Feat] Fixing the twoFa

--- app/back-end/authentication/views.py
+++ app/back-end/authentication/views.py
@@ -136,15 +136,7 @@ class Control2Fa(APIView):
             return Response({'url': url_code, 'email': user_obj.email}, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({'error': {'User does not exist'}}, status=status.HTTP_400_BAD_REQUEST)
-        # serializer = self.serializer_class(data = request.data)
-        # if serializer.is_valid():
-        #     user = serializer.validated_data
-        #     refresh = RefreshToken.for_user(user)
-        #     return Response({
-        #         'refresh': str(refresh),
-        #         'access': str(refresh.access_token)
-        #     }, status = status.HTTP_200_OK)
-        # return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
 
 
 @extend_schema_view(
