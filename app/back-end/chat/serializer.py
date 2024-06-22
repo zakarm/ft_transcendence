07011d6227029e1a60157db9f455d61a35fbad8e
zakarm <zakariaemrabet1@gmail.com>
2024-06-22 10:26:12 +0100

    [Feat] Add frindship id to chat

--- app/back-end/chat/serializer.py
+++ app/back-end/chat/serializer.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 from .models import Messages
+from dashboards.models import Friendship
 from django.db.models import F, Q
 from authentication.models import User
 
 class MessageSerializer(serializers.ModelSerializer):
+    chat_id = serializers.SerializerMethodField()
     class Meta:
         model = Messages
         fields = '__all__'
-
+    
+    def get_chat_id(self, obj):
+        chat_id = Friendship.objects.filter(Q(user_from = obj.user_one, user_to = obj.user_two) | 
+                                            Q(user_from = obj.user_two, user_to = obj.user_one))
+        return chat_id[0].freindship_id if chat_id else None
+    
 class UserMessageSerializer(serializers.ModelSerializer):
     messages = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ('id', 'username', 'messages')
+        fields = ('username', 'messages')
     
     def get_messages(self, obj):
         messages = Messages.objects.filter(Q(user_one = obj) | Q(user_two = obj))
