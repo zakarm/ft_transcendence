4881f8058c208cb516ead4b5813fffdd5fcb829b
zakarm <mrabet.zakariae21@ump.ac.ma>
2024-07-03 23:39:24 +0100

    [Fix] Handle authentication internal server errors

--- app/back-end/ft_transcendence/urls.py
+++ app/back-end/ft_transcendence/urls.py
@@ -15,35 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
-# from django.urls import path, include
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-#     TokenVerifyView,
-# )
-# from drf_spectacular.views import (
-#     SpectacularAPIView,
-#     SpectacularRedocView,
-#     SpectacularSwaggerView,
-# )
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("api/", include("authentication.urls")),
-#     path("api/", include("dashboards.urls")),
-#     path("api/", include("game.urls")),
-#     path("api/", include("game.urls")),
-#     path("api/", include("compu_ai.urls")),
-#     path("api/token", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-#     path("api/refresh", TokenRefreshView.as_view(), name="token_refresh"),
-#     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-#     path( "api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"),  name="swagger" ),
-#     path( "api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"),  name="redoc" ),
-#     path("", include("django_prometheus.urls")),
-#     path("api/", include("chat.urls")),
-# ]
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import (
