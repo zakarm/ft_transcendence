92a5427917bfed3ebffb491f63cff27d709b0c20
zakarm <mrabet.zakariae21@ump.ac.ma>
2024-07-09 16:11:25 +0100

    [Feat] Add refresh token for authchecker

--- app/front-end/src/components/authChecker.tsx
+++ app/front-end/src/components/authChecker.tsx
@@ -6,13 +6,35 @@ import Cookies from 'js-cookie';
 import Spinner from 'react-bootstrap/Spinner';
 import styles from './styles/authChecker.module.css';
 import MainContainer from './mainContainer';
-import { toast } from 'react-toastify';
 
 const AuthChecker = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter();
     const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
     useEffect(() => {
+
+        const refreshAccessToken = async (refreshToken: string) => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_HOST}/api/refresh`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ refresh: refreshToken }),
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    Cookies.set('access', data.access);
+                    return data.access;
+                } else {
+                    return null;
+                }
+            } catch (error) {
+                console.error(`Error refreshing token: ${error}`);
+                return null;
+            }
+        };
+
         const authentication = async () => {
             try {
                 const access = Cookies.get('access');
@@ -29,8 +51,22 @@ const AuthChecker = ({ children }: { children: React.ReactNode }) => {
                     if (response.ok) {
                         setIsAuthenticated(true);
                     } else {
-                        setIsAuthenticated(false);
-                        router.push('/sign-in');
+                        if (response.status === 401) {
+                            const refresh = Cookies.get('refresh');
+                            if (refresh) {
+                                const newAccess = await refreshAccessToken(refresh);
+                                if (newAccess) {
+                                    Cookies.set('access', newAccess, { path : '/' });
+                                    setIsAuthenticated(true);
+                                } else {
+                                    setIsAuthenticated(false);
+                                    router.push('/sign-in');
+                                }
+                            } else {
+                                setIsAuthenticated(false);
+                                router.push('/sign-in');
+                            }
+                        }
                     }
                 } else {
                     setIsAuthenticated(false);
@@ -41,7 +77,7 @@ const AuthChecker = ({ children }: { children: React.ReactNode }) => {
             }
         };
         authentication();
-    }, []);
+    }, [router]);
 
     if (isAuthenticated === null) {
         return (
